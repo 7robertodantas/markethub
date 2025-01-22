5e7cb38b582a031222a5fb1d2fe9fdcbb62bfdfb
herlmanoel <herlmanoelfernandes@gmail.com>
2025-01-22 19:22:02 -0300

    feat: remove findByUserId method from ProductRepository

--- product/src/main/java/br/ufrn/imd/markethub/service/product/repository/ProductRepository.java
+++ product/src/main/java/br/ufrn/imd/markethub/service/product/repository/ProductRepository.java
@@ -8,5 +8,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
-    Page<Product> findByUserId(UUID userId, Pageable pageable);
 }
