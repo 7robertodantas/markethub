5d27456a28b8e0bff337750ccb139d0fb377d721
herlmanoel <herlmanoelfernandes@gmail.com>
2025-01-22 19:05:24 -0300

    feat: add userId

--- product/src/main/java/br/ufrn/imd/markethub/service/product/domain/Product.java
+++ product/src/main/java/br/ufrn/imd/markethub/service/product/domain/Product.java
@@ -15,6 +15,9 @@ public class Product {
     @GeneratedValue
     private UUID id;
 
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
     @Column(name = "name", nullable = false)
     private String name;
 
