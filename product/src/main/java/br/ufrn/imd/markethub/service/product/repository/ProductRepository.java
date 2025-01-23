c2a6ba8bc8957833fbfe1c9e2f8e9b86fa18265b
herlmanoel <herlmanoelfernandes@gmail.com>
2025-01-23 07:59:32 -0300

    feat: remover importações não utilizadas no ProductRepository

--- product/src/main/java/br/ufrn/imd/markethub/service/product/repository/ProductRepository.java
+++ product/src/main/java/br/ufrn/imd/markethub/service/product/repository/ProductRepository.java
@@ -1,8 +1,6 @@
 package br.ufrn.imd.markethub.service.product.repository;
 
 import br.ufrn.imd.markethub.service.product.domain.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
